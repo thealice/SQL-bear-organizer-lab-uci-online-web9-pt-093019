e2185c41af40355764cef7f36d5cec94fa60e5bd
Alice Freda <alicefreda@gmail.com>
2019-12-11 23:03:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * bears WHERE bears.age = 30 && bears.gender = 'M' && bears.temperament = 'aggressive'"
 end
